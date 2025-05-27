329d13db0a667e5a0424dbdff9658f35deb66878
Matthew Mckee <matthewmckee04@yahoo.co.uk>
2025-05-27 18:29:11 +0100

    Update workflow

--- crates/karva_python/src/lib.rs
+++ crates/karva_python/src/lib.rs
@@ -1,3 +1,4 @@
+use karva::main as karva_main;
 use pyo3::prelude::*;
 
 /// Formats the sum of two numbers as string.
@@ -6,9 +7,16 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
     Ok((a + b).to_string())
 }
 
+#[pyfunction]
+fn karva_run() -> PyResult<()> {
+    karva_main();
+    Ok(())
+}
+
 /// A Python module implemented in Rust.
 #[pymodule]
 fn _karva(_py: Python<'_>, m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(karva_run, m)?)?;
     Ok(())
 }
