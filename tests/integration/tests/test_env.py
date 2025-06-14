9d5069718991dc4c6fd660e53960827a81e8b9ec
Matthew Mckee <matthewmckee04@yahoo.co.uk>
2025-06-14 16:49:52 +0100

    Debug

--- tests/integration/tests/test_env.py
+++ tests/integration/tests/test_env.py
@@ -124,5 +124,11 @@ exit_code: {self.exit_code}
             )
         if isinstance(other, str):
             other_snapshot = CommandSnapshot.from_str(other, self.project_dir)
-            return self == other_snapshot
+            res = self == other_snapshot
+            if not res:
+                print("Expected--------------------------------")
+                print(other_snapshot.format())
+                print("\nActual--------------------------------")
+                print(self.format())
+            return res
         return False
