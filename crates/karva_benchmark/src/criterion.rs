7a6cd0268a613ce5f5b6e6297d7759deac1a6620
Matthew Mckee <matthewmckee04@yahoo.co.uk>
2025-10-27 13:22:26 +0000

    Fix walltime benchmarks (#167)
    ## Summary
    
    Removed the codspeed feature to fix the walltime benchmark.

--- crates/karva_benchmark/src/criterion.rs
+++ crates/karva_benchmark/src/criterion.rs
@@ -1,8 +1,1 @@
-#[cfg(not(codspeed))]
-pub use criterion::*;
-
-#[cfg(not(codspeed))]
-pub type BenchmarkGroup<'a> = criterion::BenchmarkGroup<'a, measurement::WallTime>;
-
-#[cfg(codspeed)]
 pub use codspeed_criterion_compat::*;
