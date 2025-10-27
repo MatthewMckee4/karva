102735d97a1c63e02ce2920a8b7dd53bf433bed7
Matthew Mckee <matthewmckee04@yahoo.co.uk>
2025-10-27 13:01:08 +0000

    Fix walltime benchmarks

--- crates/karva_benchmark/src/criterion.rs
+++ crates/karva_benchmark/src/criterion.rs
@@ -1,8 +1,4 @@
 #[cfg(not(codspeed))]
-pub use criterion::*;
-
-#[cfg(not(codspeed))]
 pub type BenchmarkGroup<'a> = criterion::BenchmarkGroup<'a, measurement::WallTime>;
 
-#[cfg(codspeed)]
 pub use codspeed_criterion_compat::*;
