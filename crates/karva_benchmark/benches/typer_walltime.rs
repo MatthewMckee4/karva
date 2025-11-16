af03435f40192823d1465964bafde9eb7c2716db
Matthew Mckee <matthewmckee04@yahoo.co.uk>
2025-11-16 19:38:59 +0000

    Separate project walltime benchmarks

---
+++ crates/karva_benchmark/benches/typer_walltime.rs
@@ -0,0 +1,16 @@
+use karva_benchmark::{
+    criterion::{Criterion, criterion_group, criterion_main},
+    walltime::{ProjectBenchmark, bench_project},
+};
+
+fn typer(criterion: &mut Criterion) {
+    use karva_test::real_world_projects::typer_project;
+
+    let benchmark = ProjectBenchmark::new(typer_project());
+
+    bench_project(&benchmark, criterion);
+}
+
+criterion_group!(project, typer);
+
+criterion_main!(project);
