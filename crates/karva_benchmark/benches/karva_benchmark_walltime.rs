46e6e2f0ce1350b24d7dbf01b464d8238c671afa
MatthewMckee4 <matthewmckee04@yahoo.co.uk>
2026-02-03 00:24:50 +0000

    Update num benchmark runs

--- crates/karva_benchmark/benches/karva_benchmark_walltime.rs
+++ crates/karva_benchmark/benches/karva_benchmark_walltime.rs
@@ -2,7 +2,7 @@ use divan::{Bencher, bench};
 use karva_benchmark::walltime::{ProjectBenchmark, bench_project, warmup_project};
 use karva_projects::real_world_projects::KARVA_BENCHMARK_PROJECT;
 
-#[bench(sample_size = 4, sample_count = 5)]
+#[bench(sample_size = 3, sample_count = 3)]
 fn karva_benchmark(bencher: Bencher) {
     let benchmark = ProjectBenchmark::new(KARVA_BENCHMARK_PROJECT.clone());
 
