8e1b04aa24e591b64a88d2ded5e262966208ab12
Matthew Mckee <matthewmckee04@yahoo.co.uk>
2026-08-04 03:12:07 +0100

    .

--- crates/karva/src/lib.rs
+++ crates/karva/src/lib.rs
@@ -1,10 +1,12 @@
 //! Top-level CLI entry point shared by the `karva` binary and integration tests.
 
 use std::ffi::OsString;
+use std::io;
 use std::io::stdout;
 
 use anyhow::Context;
 use clap::{CommandFactory, Parser};
+use colored::Colorize;
 use karva_cli::{Args, Command};
 
 pub use karva_cli::ExitStatus;
@@ -14,16 +16,32 @@ mod utils;
 
 pub fn karva_main(f: impl FnOnce(Vec<OsString>) -> Vec<OsString>) -> ExitStatus {
     run(f).unwrap_or_else(|error| {
-        if karva_logging::error_chain_contains_broken_pipe(error.chain()) {
+        use io::Write;
+
+        // Exit "gracefully" on broken pipe errors.
+        //
+        // See: https://github.com/BurntSushi/ripgrep/blob/bf63fe8f258afc09bae6caa48f0ae35eaf115005/crates/core/main.rs#L47C1-L61C14
+        if error.chain().any(|cause| {
+            cause
+                .downcast_ref::<io::Error>()
+                .is_some_and(|ioerr| ioerr.kind() == io::ErrorKind::BrokenPipe)
+        }) {
             return ExitStatus::Success;
         }
 
-        let mut stderr = std::io::stderr().lock();
-        if let Err(err) = karva_logging::write_error_chain(&mut stderr, error.chain()) {
-            if err.kind() == std::io::ErrorKind::BrokenPipe {
-                return ExitStatus::Success;
-            }
+        // Use `writeln` instead of `eprintln` to avoid panicking when the stderr pipe is broken.
+        let mut stderr = io::stderr().lock();
+
+        // This communicates that this isn't a linter error but karva itself hard-errored for
+        // some reason (e.g. failed to resolve the configuration)
+        writeln!(stderr, "{}", "karva failed".red().bold()).ok();
+        // Currently we generally only see one error, but e.g. with io errors when resolving
+        // the configuration it is help to chain errors ("resolving configuration failed" ->
+        // "failed to read file: subdir/pyproject.toml")
+        for cause in error.chain() {
+            writeln!(stderr, "  {} {cause}", "Cause:".bold()).ok();
         }
+
         ExitStatus::Error
     })
 }
